02bc59def9817fe189f7041908892175d6f965a7
Peter Williams <pedroeldiablo@yahoo.com>
2019-10-23 16:19:53 +0100

    initial pie chart for first year

--- app.js
+++ app.js
@@ -22,3 +22,21 @@ d3.select('svg')
   .append('g')
   .attr('transform', 'translate(' + width / 2 + ', ' + height /2 + ')')
   .classed('chart', true);
+
+var arcs = d3.pie()
+  .value(d => d.births)
+  (yearData);
+
+var path = d3.arc()
+  .outerRadius(width / 2 - 10)
+  .innerRadius( width / 4 );
+
+d3.select('.chart')
+  .selectAll('.arc')
+  .data(arcs)
+  .enter()
+  .append('path')
+  .classed('arc', true)
+  .attr('fill', d => colorScale(d.data.continent))
+  .attr('stroke', 'black')
+  .attr('d', path);
